dcd9b629b4aa63b4e84e9470738269148f5799e2
Simerus Mahesh <63352151+SimerusM@users.noreply.github.com>
2024-08-24 14:22:20 -0400

    meeting api endpoint and flask env set up

--- api-server/app.py
+++ api-server/app.py
@@ -1,26 +1,15 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, render_template, request
+import uuid
 
 app = Flask(__name__)
 
-# A simple route for the home page
-@app.route('/')
-def home():
-    return "Welcome to the Flask App!"
-
-# A route that returns JSON data
-@app.route('/api/data', methods=['GET'])
-def get_data():
-    sample_data = {
-        "message": "Hello, this is your Flask API!",
-        "status": "success"
-    }
-    return jsonify(sample_data)
-
-# A route that accepts POST requests with JSON data
-@app.route('/api/data', methods=['POST'])
-def receive_data():
-    data = request.get_json()
-    return jsonify({"received_data": data, "status": "data received"}), 201
+# Route to generate a unique meeting link
+@app.route('/api/create-meeting', methods=['POST'])
+def create_meeting():
+    # Generate a unique meeting ID
+    meeting_id = str(uuid.uuid4())
+    
+    return jsonify({'meeting_id': meeting_id})
 
 if __name__ == '__main__':
     app.run(debug=True)
