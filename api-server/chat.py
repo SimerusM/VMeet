e108c99c9197034b20489e6863f6e461dab18742
ericz <ericzh1616@gmail.com>
2024-08-27 17:41:45 -0400

    refactor api-server

--- api-server/chat.py
+++ api-server/chat.py
@@ -1,58 +1,24 @@
 from flask import jsonify, request
-from flask_socketio import emit, join_room
-import uuid
+from flask_socketio import emit
 
-def setup_chat_routes(app, session_storage, log_message):
-    @app.route('/api/create-meeting', methods=['POST'])
-    def create_meeting():
-        meeting_id = str(uuid.uuid4())
-        data = request.get_json()
-        username = data['username']
-        session_storage[meeting_id] = {
-            'host': username,
-            'users': [],
-            'chat_history': []
-        }
-        log_message('INFO', f'User {username} created a new meeting', meeting_id)
-        return jsonify({'meeting_id': meeting_id})
-
+def setup_chat(app, socketio, session_storage, log_message):
     @app.route('/api/chat_history/<meeting_id>', methods=['GET'])
     def get_chat_history(meeting_id):
-        if meeting_id in session_storage:
-            return jsonify(session_storage[meeting_id]['chat_history'])
-        else:
+        if meeting_id not in session_storage:
             return jsonify({'error': 'Meeting ID not found'}), 404
-
-    @app.route('/api/session/<meeting_id>', methods=['GET'])
-    def get_users(meeting_id):
-        if meeting_id in session_storage:
-            return jsonify(session_storage[meeting_id])
-        else:
-            return jsonify({'error': 'Meeting ID not found'}), 404
-
-def setup_chat_sockets(socketio, session_storage, log_message):
-    @socketio.on('join')
-    def handle_join(data):
-        if 'username' not in data or 'meeting_id' not in data:
-            log_message('ERROR', f'Join request missing username or meeting ID: {data}')
-            return
-        meeting_id = data['meeting_id']
-        username = data['username']
-        join_room(meeting_id)
-        session = session_storage[meeting_id]
-        session['users'].append(username)
-
-        log_message("DEBUG", "Session storage: ", session_storage)
-        log_message('INFO', f'User {username} joined the meeting', meeting_id)
-        emit('user_joined', {'username': username, 'meeting_id': meeting_id}, room=meeting_id)
+        return jsonify(session_storage[meeting_id]['chat_history'])
 
     @socketio.on('chat_message')
     def handle_chat_message(data):
         meeting_id = data['meeting_id']
         sender = data['sender']
         message = data['text']
+
         if meeting_id not in session_storage:
+            log_message('ERROR', f'Meeting ID {meeting_id} not found', meeting_id)
+            emit('error', {'message': 'Meeting ID not found'}, to=request.sid)
             return
+
         session_storage[meeting_id]['chat_history'].append({'sender': sender, 'text': message})
         log_message('INFO', f'User {sender} sent: {message}', meeting_id)
         emit('chat_message', {'sender': sender, 'text': message}, room=meeting_id)
--- api-server/webrtc.py
+++ api-server/webrtc.py
@@ -1,17 +1,17 @@
 from flask_socketio import emit
 
-def setup_webrtc_sockets(socketio):
+def setup_webrtc(app, socketio, session_storage, log_message):
     @socketio.on('offer')
     def handle_offer(data):
-        meeting_id = data['meeting_id']
-        emit('offer', data, room=meeting_id)
+        to_sid = session_storage[data['meeting_id']]['users'][data['to']]
+        emit('offer', data, room=to_sid)
 
     @socketio.on('answer')
     def handle_answer(data):
-        meeting_id = data['meeting_id']
-        emit('answer', data, room=meeting_id)
+        to_sid = session_storage[data['meeting_id']]['users'][data['to']]
+        emit('answer', data, room=to_sid)
 
     @socketio.on('ice_candidate')
     def handle_ice_candidate(data):
-        meeting_id = data['meeting_id']
-        emit('ice_candidate', data, room=meeting_id)
+        to_sid = session_storage[data['meeting_id']]['users'][data['to']]
+        emit('ice_candidate', data, room=to_sid)
