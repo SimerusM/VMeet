9f6731c51646bcfba4cc79d89a180e3081edb743
Simerus Mahesh <63352151+SimerusM@users.noreply.github.com>
2024-08-28 21:36:28 -0400

    backend debugger + vid/aud toggle

--- api-server/webrtc.py
+++ api-server/webrtc.py
@@ -1,7 +1,18 @@
 from flask import request
 from flask_socketio import emit
 
+class DebugTools:
+    @staticmethod
+    def log_transport(self):
+        '''
+        Dev Function to log the transport method used in the WebSocket connection.
+        '''
+        transport = request.args.get('transport', 'unknown')
+        self.log_message('DEBUG', f'Connection transport: {transport}')
+
+
 def setup_webrtc(app, socketio, session_storage, log_message):
+
     @socketio.on('offer')
     def handle_offer(data):
         to_sid = session_storage[data['meeting_id']]['users'][data['to']]
